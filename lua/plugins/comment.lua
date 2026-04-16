ca9ade42e6a95ff28ed4a8cc63fe0899ea1309ae
Prerak1310 <preraksangwan@gmail.com>
2026-04-16 12:37:46 +0530

    testing conflicts

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,8 @@
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
-	"numToStr/Comment.nvim",
+	"/Comment.nvim",
 	opts = {
 		-- add any options here
+		-- laksdjflaksdjflkasjdflkasjdfl
 	},
 }
