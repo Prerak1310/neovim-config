cf1080cd582e94c8f2f38772d0bc0b91c912a9dd
Prerak1310 <preraksangwan@gmail.com>
2026-04-24 11:34:42 +0530

    oil.nvim added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,4 @@ vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua lsp_definitions<cr>")
 vim.keymap.set("n", "<leader>g", "<cmd>FzfLua grep<cr>")
 vim.keymap.set("n", "<leader>cg", "<cmd>FzfLua git_commits<cr>")
 
-vim.keymap.set("n", "e", "<cmd>Ex<cr>")
+vim.keymap.set("n", "e", "<cmd>Oil<cr>")
